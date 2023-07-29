ec60e3d61c3356feb460f079d8bb7270494dca18
Flavia Mendes <flaviamendesdasilva99@gmail.com>
2023-07-29 02:02:31 -0300

    add formato correto de data

--- src/schemas/schemas.clientes.js
+++ src/schemas/schemas.clientes.js
@@ -5,6 +5,6 @@ export const clientesSchema = joi.object({
 
   name: joi.string().required(),
   phone: joi.string().required().min(10).max(11).required(),
-  cpf: joi.string().length(11) .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  birthday: joi.date().format('YYYY-MM-DD').required()
+  cpf: joi.string().length(11) .pattern(/^\d+$/).required(),
+  birthday: joi.date().iso().required()
 })
