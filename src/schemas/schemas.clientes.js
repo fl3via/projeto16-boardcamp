62c7b59e9db734198e4096e45c63b4df6ea7f1fd
Flavia Mendes <flaviamendesdasilva99@gmail.com>
2023-07-29 02:12:44 -0300

    add data

--- src/schemas/schemas.clientes.js
+++ src/schemas/schemas.clientes.js
@@ -1,10 +1,12 @@
-import joi from 'joi'
+import joiBase from 'joi'
 import joiDate from '@joi/date'
+
+const joi = joiBase.extend(joiDate)
 
 export const clientesSchema = joi.object({
 
   name: joi.string().required(),
   phone: joi.string().required().min(10).max(11).required(),
   cpf: joi.string().length(11) .pattern(/^\d+$/).required(),
-  birthday: joi.date().iso().required()
+  birthday: joi.date().format('YYYY-MM-DD').required()
 })
