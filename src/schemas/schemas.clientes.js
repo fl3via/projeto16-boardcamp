59f853105ebf8ab90dc28c6a03d596e6a92fd55b
Flavia Mendes <flaviamendesdasilva99@gmail.com>
2023-07-29 01:53:09 -0300

    add requisito de validação de data

--- src/schemas/schemas.clientes.js
+++ src/schemas/schemas.clientes.js
@@ -1,9 +1,10 @@
 import joi from 'joi'
+import joiDate from '@joi/date'
 
 export const clientesSchema = joi.object({
 
   name: joi.string().required(),
   phone: joi.string().required().min(10).max(11).required(),
-  cpf: joi.string().length(11).required(),
-  birthday: joi.date().required()
+  cpf: joi.string().length(11) .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  birthday: joi.date().format('YYYY-MM-DD').required()
 })
