aa6f3236efc73953896b3116aca8b3b5bcc5a97a
Flavia Mendes <flaviamendesdasilva99@gmail.com>
2023-07-29 02:20:10 -0300

    .

--- src/schemas/schemas.clientes.js
+++ src/schemas/schemas.clientes.js
@@ -1,12 +1,9 @@
-import joiBase from 'joi'
-import joiDate from '@joi/date'
-
-const joi = joiBase.extend(joiDate)
+import joi from 'joi'
 
 export const clientesSchema = joi.object({
 
   name: joi.string().required(),
   phone: joi.string().required().min(10).max(11).required(),
   cpf: joi.string().length(11) .pattern(/^\d+$/).required(),
-  birthday: joi.date().format('YYYY-MM-DD').required()
+  birthday: joi.date().iso().required()
 })
