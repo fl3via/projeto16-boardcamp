fcdb836ec2b23512d896a3dacbd49e795a822a0f
Flavia Mendes <flaviamendesdasilva99@gmail.com>
2023-07-30 02:34:53 -0300

    add requisito criar aluguel rota post/rentals

--- src/controllers/aluguel.controllers.js
+++ src/controllers/aluguel.controllers.js
@@ -1,5 +1,6 @@
-import{ db }from '../database/database.connection.js'
+import { db } from '../database/database.connection.js'
 import { aluguelSchema } from '../schemas/schemas.aluguel.js'
+import dayjs from 'dayjs'
 
 
 
@@ -14,10 +15,10 @@ export async function getListarAlugueis(req, res) {
             ...rest,
             customers: { id: customerId, name: customerName },
             game: { id: gameId, name: gameName }
-          }))
-          
-          res.send(result)
-          
+        }))
+
+        res.send(result)
+
     } catch (err) {
         res.status(500).send(err.message)
     }
@@ -28,20 +29,39 @@ export async function getListarAlugueis(req, res) {
 export async function postInserirAluguel(req, res) {
     const { customerId, gameId, daysRented } = req.body
 
-    const validation = aluguelSchema.validate(req.body, {abortEarly: false})
-
-    if (validation.error) {
-        const errors = validation.error.details.map((detail) => detail.message)
-        return res.status(400).send(errors)
-    }
-
     try {
-        
+        const customerResult = await db.query(`SELECT * FROM customers WHERE id = $1`, [customerId])
+        const gameResult = await db.query(`SELECT * FROM games WHERE id = $1`, [gameId])
 
+        if (customerResult.rowCount === 0 || gameResult.rowCount === 0) {
+            return res.sendStatus(400)
+        }
+
+        const rentalCountResult = await db.query(`
+                SELECT COUNT(*) as count FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL
+            `, [gameId])
+
+        const stockTotal = gameResult.rows[0].stockTotal
+        const rentalCount = rentalCountResult.rows[0].count
+
+        if (rentalCount >= stockTotal) {
+            return res.sendStatus(400)
+        }
+
+        const rentDate = new Date().toISOString().slice(0, 10);
+        const originalPrice = daysRented * gameResult.rows[0].pricePerDay
+
+        await db.query(`
+                INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "originalPrice", "returnDate", "delayFee")
+                VALUES ($1, $2, $3, $4, $5, null, null);
+            `, [customerId, gameId, daysRented, rentDate, originalPrice])
+
+        res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
-    } 
-} 
+    }
+}
+
 
 
 export async function postFinalizarAluguel(req, res) {
@@ -59,3 +79,14 @@ export async function apagarAluguel(req, res) {
         res.status(500).send(err.message)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
